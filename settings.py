af44435545c9a3500b386ff7f37b155796491b23
Anthony Arendt <arendta@uw.edu>
2016-01-03 08:37:42 -0800

    Delete settings.py

--- settings.py
+++
@@ -1,2 +0,0 @@
-defaulthost = {"host":'localhost',"dbname":"sandbox","user":'arendta',"password":'Z2xBJGlFcjE='}
-#defaulthost = {"host":'localhost',"dbname":"sandbox","user":'sai',"password":'test'}
