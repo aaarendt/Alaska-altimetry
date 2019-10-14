f9381ea98f830745e374c5041979139062fe3c51
geohackweek <arendta@uw.edu>
2019-10-14 10:50:05 -0700

    initial commit

---
+++ export.sql
@@ -0,0 +1,19 @@
+-- export glacier wide mass balance (m w.e. / yr) of final results of Larsen et al (2015)
+copy ( 
+select rgiid, a3.glimsid, massbal from modern
+join
+(select glimsid, massbal from ergi
+join 
+(select ergiid, s/a * 0.85 as massbal
+from
+(select ergiid,sum(mean*area) as s,sum(area) as a from public.altimetryextrapolation
+group by "ergiid"
+order by "ergiid") as a1) as a2
+on
+(ergi.ergiid = a2.ergiid)) as a3
+on 
+(modern.glimsid = a3.glimsid)
+) as a4
+where massbal is null;
+)
+TO 'C:\work\AlaskaAltimetry.csv' DELIMITER ',' CSV HEADER;
