3e1d01c148f833346362ed2adbe19c4683f34450
aaarendt <arendta@uw.edu>
2019-12-27 09:45:55 -0800

    add some documentation

--- sql/new_LAMB_table.sql
+++ sql/new_LAMB_table.sql
@@ -1,6 +1,5 @@
 -- this code is run just once when creating a new table in the database to store the Laser Altimetry Mass Balance (lamb) data
 
-CREATE TABLE lamb (lambid serial PRIMARY KEY, glimsid character varying(14), date1 date, date2 date, \
-               interval smallint, volmodel real, vol25diff real,vol75diff real, balmodel real, bal25diff real, \
-               bal75diff real,e integer[],dz real[],dz25 real[],dz75 real[],aad real[],masschange real[], \
-               massbal real[],numdata integer[]);
+-- run this code with admin level access
+
+CREATE TABLE lamb (lambid serial PRIMARY KEY, glimsid character varying(14), date1 date, date2 date, interval smallint, volmodel real, vol25diff real,vol75diff real, balmodel real, bal25diff real, bal75diff real,e integer[],dz real[],dz25 real[],dz75 real[],aad real[],masschange real[], massbal real[],numdata integer[]);
